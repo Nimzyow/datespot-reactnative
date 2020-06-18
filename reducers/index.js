f43fe43d43b6a7af3b2143cb9b600f5ae5a29d85
Nimzyow <n_soufiani@hotmail.com>
2020-06-18 23:57:39 +0100

    tested for spots page and passigng tests

--- reducers/index.js
+++ reducers/index.js
@@ -4,5 +4,5 @@ import spotReducer from './spotReducer';
 
 export default combineReducers({
   auth: authReducer,
-  spot: spotReducer,
+  spot: spotsReducer,
 });
--- screens/spots/Spots.js
+++ screens/spots/Spots.js
@@ -1,11 +1,18 @@
 import React, {useEffect} from 'react';
 import {View, Text} from 'react-native';
-import {Card, Container} from 'native-base';
+import {Card, CardItem, Container, Content} from 'native-base';
 import {connect} from 'react-redux';
 
 import {loadUser} from '../../actions/auth';
+import {getSpots} from '../../actions/spots';
 
-export const Spots = ({navigation, auth: {isAuthenticated}, loadUser}) => {
+export const Spots = ({
+  navigation,
+  auth: {isAuthenticated},
+  loadUser,
+  spot: {spots},
+  getSpots,
+}) => {
   useEffect(() => {
     loadUser();
     if (!isAuthenticated) {
@@ -13,19 +20,39 @@ export const Spots = ({navigation, auth: {isAuthenticated}, loadUser}) => {
 
       navigation.navigate('home');
     }
+    getSpots();
   }, []);
   return (
     <View accessibilityLabel="spotsContainer">
-      <Container accessibilityLabel="spotItemsContainer" />
+      <Container accessibilityLabel="spotItemsContainer">
+        <Content>
+          <Card>
+            {spots !== null ? (
+              spots.map(spot => {
+                return (
+                  <CardItem key={spot._id} accessibilityLabel="spotItemElement">
+                    <Text>{spot.title}</Text>
+                  </CardItem>
+                );
+              })
+            ) : (
+              <View accessibilityLabel="loading">
+                <Text>Loading</Text>
+              </View>
+            )}
+          </Card>
+        </Content>
+      </Container>
     </View>
   );
 };
 
 const mapStateToProps = state => ({
   auth: state.auth,
+  spot: state.spots,
 });
 
 export default connect(
   mapStateToProps,
-  {loadUser},
+  {loadUser, getSpots},
 )(Spots);
